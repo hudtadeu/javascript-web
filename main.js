e2f0b50a9f8980720aa1d4bf37d1ba1e83c91866
Hudson Tadeu <hudinvesting@gmail.com>
2023-07-26 12:39:47 -0300

    Tocar som

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-alert('Olá mundo');
+function tocaSomPom() {
+  document.querySelector('#som_tecla_pom').play();
+}
+
+document.querySelector('.tecla_pom');
+
+function tocaSomClap() {
+  document.querySelector('#som_tecla_clap').play();
+}
+
+document.querySelector('.tecla_clap');
