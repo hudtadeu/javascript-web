c7ba9bdbacf4a3a4f836f8f10c494302f52855f1
Hudson Tadeu <hudinvesting@gmail.com>
2023-08-01 20:08:57 -0300

    Melhorando o código

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
 function tocaSom(seletorAudio) {
   const elemento = document.querySelector(seletorAudio).play();
 
-  if (elemento === null) {
-    console.log('Elemento não encontrado');
+  if (elemento && elemento.localName === 'audio') {
+    elemento.play();
   }
 
-  if (elemento != null) {
-    if (elemento.localName === 'audio') {
-      elemento.play();
-    }
+  else {
+    console.log('Elemento não encontrado ou seletor inválido');
   }
 }
 
@@ -28,13 +26,13 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
   }
 
   tecla.onkeydown = function (evento) {
-    
-    if (evento.code === 'Space'|| evento.code === 'Enter') {
+
+    if (evento.code === 'Space' || evento.code === 'Enter') {
       tecla.classList.add('ativa');
     }
 
-  tecla.onkeyup = function () {
-    tecla.classList.remove('ativa');
+    tecla.onkeyup = function () {
+      tecla.classList.remove('ativa');
+    }
   }
-}
 }
